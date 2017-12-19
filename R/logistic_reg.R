0bcea6039389b48f0a2a2acf479ded7285cc0bf5
gabeleibo <gabriel.leibovich@gmail.com>
2017-12-19 18:05:23 +0100

    Update logistic_reg.R

--- R/logistic_reg.R
+++ R/logistic_reg.R
@@ -31,8 +31,8 @@ q = dim(X)[2] #number of HUE values per picture
 
 #Splitting the data in training and testing data
 
-set.seed(12345)
-train_p <- 0.9
+set.seed(12345) #the same used in Neaural Networks
+train_p <- 0.9 #the same used in Neaural Networks
 TrainingSize <- floor(train_p*nrow(X))
 SelectRow <- sample(seq_len(nrow(X)), size = TrainingSize) 
 
@@ -41,6 +41,7 @@ y_train = y[c(SelectRow)]
 X_test = X[-c(SelectRow),]
 y_test = y[-c(SelectRow)]
 
+# training data characteristics
 n_train = dim(X_train)[1]
 q_train = dim(X_train)[2]
 
@@ -48,7 +49,7 @@ q_train = dim(X_train)[2]
 
 #creating empty matrix
 num_labels = 3
-beta_one_vs_all = matrix(0,q_train + 1, num_labels)
+beta_one_vs_all = matrix(0,q_train + 1, num_labels) #betas for each colour in seperate columns
 
 
 for (c in 1:num_labels) {
@@ -97,7 +98,7 @@ for (l in 1:num_labels) {
 
 ##################################################################################### 
 ###LOGISTIC REGRESSION (one-vs-all) classifier for color recognition project 
-###WITH DATA ADAPTATION (Aggregation)
+###WITH INDEPENDENT VARIABLES ADJUSTMENTS (Aggregation)
 
 #The results in previous part depends a lot on the picture colors' place. If the picture has
 #its key colors in the corner only, the algorithm will be weak to predict it. In order to deal with
@@ -109,24 +110,18 @@ meanexcludingzeros <- function(inputdata) {
 }
 
 X_Means = apply(X,1,meanexcludingzeros) #averages of HUE values per picture
-#Since the HUE values are very dispersed, the mean is very sensitive to small numbers, especially 
-#for red color. Therefore median excluding noise might be more applicable
-
-mediananexcludingnoise <- function(inputdata) {
-  #Calculate median values excluding the noise for the data matrix
-  if (all(inputdata<=5)) 0 else median(inputdata[inputdata>5])
-}
-
-X_Median = apply(X,1,mediananexcludingnoise) #medians of HUE values per picture
+#Since the HUE values are very dispersed (there are a lot of observations in both ends of HUE value spectrum),
+#the mean is very sensitive, especially 
+#for the red color. Median did not perform better. 
 
 MostCommonValue <- function(inputdata) {
-  #Find the most common HUE value excluding the noise (HUE numbers less or equal 5) for the data matrix
-  if (all(inputdata<=5)) 0 else as.numeric(names(which.max(table(inputdata[inputdata>5]))))
+  #Find the most common HUE value excluding the noise (HUE numbers of white colour (0)) for the data matrix
+  if (all(inputdata==0)) 0 else as.numeric(names(which.max(table(inputdata[inputdata!=0]))))
 }
 
 X_MostCommon = apply(X,1,MostCommonValue) #Most common HUE value per picture
 
-# Boxplot of Mean of HUE values per picture
+# Boxplot of Mean and most common HUE values per picture
 
 y_text = y 
 y_text[y_text == 1] <- 'Blue' #Changing numerical values to strings
@@ -134,69 +129,18 @@ y_text[y_text == 2] <- 'Red' #Changing numerical values to strings
 y_text[y_text == 3] <- 'Green' #Changing numerical values to strings
 
 
-par(mfrow=c(1,3))
+par(mfrow=c(1,2))
 boxplot(X_Means~y_text,  ylab="a) Mean", col=c('Blue', 'Green','Red'))
 
-boxplot(X_Median~y_text, xlab="The color", ylab="b) Median", col=c('Blue', 'Green','Red'))
-
-boxplot(X_MostCommon~y_text,  ylab="c) Most Common", col=c('Blue', 'Green','Red'))
+boxplot(X_MostCommon~y_text,  ylab="b) Most Common", col=c('Blue', 'Green','Red'))
 mtext("HUE values characteristics across pictures for blue, red and green colors", side = 3, line = -2.5, outer = TRUE)
 
 #Splitting the data in training and testing data
 
-X_Median_train = X_Median[c(SelectRow)]
-X_Median_test = X_Median[-c(SelectRow)]
 X_MostCommon_train = X_MostCommon[c(SelectRow)]
 X_MostCommon_test = X_MostCommon[-c(SelectRow)]
 
-q_train_alt = 1 #aggregate HUE values (by median or most common) leads to 1 explanatory variable
-
-### ONE-VS-ALL LOGISTIC REGRESSION: logistic classifier for the 3 colours using MEDIAN. 
-
-#creating empty matrix
-beta_one_vs_all_Median = matrix(0,q_train_alt + 1, num_labels)
-
-for (c in 1:num_labels) {
-  #creating one-vs-all data (with values 0 and 1), where the color in question has the value of 1 and all other colors - 0.
-  id_selected=which(y_train==c)
-  y_c = y_train
-  y_c[-id_selected] = 0  
-  y_c[id_selected] = 1
-  
-  #running logistic regression for each color
-  data = data.frame(y_c,X_Median_train)
-  model_glmfit_c = glm(y_c ~., data, start =rep(0,q_train_alt+1) ,family=binomial(link="logit"),
-                       control=list(maxit = 100, trace = FALSE) )
-  beta_glmfit_c  = model_glmfit_c$coefficients # coefficients
-  beta_glmfit_c[is.na(beta_glmfit_c)]=0
-  
-  beta_one_vs_all_Median[, c] = beta_glmfit_c # beta coefficients for each color in seperate columns
-}
-
-### Classification, Missclassification matrix and Errors
-#Training data:
-y_Median_classified = apply( cbind(rep(1,n_train), X_Median_train) %*% beta_one_vs_all_Median , 1, FUN=which.max) 
-Empirical_error_one_vs_all_Median = length(which(y_Median_classified != y_train)) / n_train
-Accuracy_Median <- 1 - Empirical_error_one_vs_all_Median
-
-misclassification_matrix_Median = matrix(0, num_labels, num_labels)
-for (i in 1:num_labels) {
-  for (j in 1:num_labels) {
-    misclassification_matrix_Median[i, j] = length(which((y_train == i) & (y_Median_classified == j))) / length(which((y_train == i)))
-  }
-}
-
-#Testing data:
-y_Median_classified_test = apply(cbind(rep(1,n_test), X_Median_test) %*% beta_one_vs_all_Median , 1, FUN=which.max)
-Empirical_error_one_vs_all_test_Median = length(which(y_Median_classified_test != y_test)) / n_test
-Accuracy_Median_test <- 1 - Empirical_error_one_vs_all_test_Median
-
-misclassification_matrix_test_Median = matrix(0,num_labels, num_labels)
-for (l in 1:num_labels) {
-  for (k in 1:num_labels) {
-    misclassification_matrix_test_Median[l, k] = length(which((y_test == l) & (y_Median_classified_test == k))) / length(which((y_test == l)))
-  }
-}
+q_train_alt = 1 #aggregate HUE values (by most common) leads to 1 explanatory variable
 
 ### ONE-VS-ALL LOGISTIC REGRESSION: logistic classifier for the 3 colours using THE MOST COMMON VALUE. 
 
@@ -246,4 +190,3 @@ for (l in 1:num_labels) {
     misclassification_matrix_test_MostCommon[l, k] = length(which((y_test == l) & (y_MostCommon_classified_test == k))) / length(which((y_test == l)))
   }
 }
-
